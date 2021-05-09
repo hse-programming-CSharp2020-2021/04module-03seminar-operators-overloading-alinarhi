d0e671c21fe97b02f18b7da74d3ed1673e10b445
Алина Архипова <aaarkhipova_6@edu.hse.ru>
2021-05-09 23:34:14 +0300

    3 с запретом 0

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -38,7 +38,7 @@ namespace Task03
             }
             set
             {
-                if (value < 0)
+                if (value <= 0)
                 {
                     throw new ArgumentException();
                 }
